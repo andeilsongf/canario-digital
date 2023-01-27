e1d2c9bbd17e150718b995b3b7f78034b765c5e0
Andeilson Ferreira <agf@Andeilsons-MacBook-Pro.local>
2023-01-27 18:19:05 -0300

    created project with tailwind theme

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="font-playfair">
+      Hello World
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange:  {
+          100: '#F27121',
+          200: '#FF4E50'
+        },
+
+        white: '#FFFFFF',
+        gray: '#A6A6A6',
+        black: '#00000'
+       
+
+      },
+
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"],
+        "playfair": ["Playfair Display", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
